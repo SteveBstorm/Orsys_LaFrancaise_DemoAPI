236c56d1fdc83f738d1d5d2d0f28addeafa51a0d
stevebstorm <steve.lorent@gmail.Com>
2023-07-19 16:13:40 +0200

    Finalisation ChatHub

--- DemoWebAPI/Hubs/ChatHub.cs
+++ DemoWebAPI/Hubs/ChatHub.cs
@@ -9,5 +9,21 @@ namespace DemoWebAPI.Hubs
         {
             await Clients.All.SendAsync("RecieveMessage", m);
         }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await SendToGroup(groupName, new Message
+            {
+                Username = "System",
+                Content = "Un nouvel utilisateur est arrivé",
+                SendDate = DateTime.Now
+            });
+        }
+
+        public async Task SendToGroup(string groupName, Message m)
+        {
+            await Clients.Group(groupName).SendAsync("RecieveFromGroup", m);
+        }
     }
 }
